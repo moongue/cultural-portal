2a92a26e9c214ce78239936eb586c9fc8488de72
Konstantin Andreev <chelvideo@mail.ru>
2020-02-24 02:45:50 +0500

    feat: add routes in App.js, add blank developers page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component, Suspense } from 'react';
+import {Switch, Route} from "react-router-dom";
 import { withTranslation } from 'react-i18next';
 import Header from './components/Header/Header';
 import Greeting from './components/Greeting/Greeting';
+import Developers from './pages/Developers/Developers';
 
 class AppEl extends Component {
   render() {
     return (
       <>
         <Header />
-        <Greeting />
+        
+        <Switch>
+          <Route exact path="/">
+            <Greeting />
+          </Route>
+          <Route path="/developers">
+            <Developers />
+          </Route>
+        </Switch>
       </>
     );
   }
---
+++ src/pages/Developers/Developers.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import Header from '../../components/Header/Header';
+import FilmmakerCardGallery from '../../components/UI/Cards/Card-gallery';
+
+class Developers extends Component {
+  render() {
+    return (
+        
+      <FilmmakerCardGallery />
+    );  
+  }
+}
+
+export default Developers;
