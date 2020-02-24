44697333b943f6410868f39913f5a15dd2b8b806
Egor <Twinkstriker@gmail.com>
2020-02-24 22:06:00 +0300

    feat: main info and structure for timeline of author

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component, Suspense } from 'react';
 import { withTranslation } from 'react-i18next';
 import Header from './components/Header/Header';
 import Greeting from './components/Greeting/Greeting';
+import AuthorOfDay from './pages/Author of day/AuthorOfDay';
 
 class AppEl extends Component {
   render() {
     return (
       <>
         <Header />
-        <Greeting />
+        {/* <Greeting /> */}
+        <AuthorOfDay  />
       </>
     );
   }
--- src/pages/Author of day/AuthorOfDay.js
+++ src/pages/Author of day/AuthorOfDay.js
@@ -1,13 +1,35 @@
 import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation, useTranslation } from 'react-i18next';
+import './AuthorOfDay.scss';
+import dataFilmDirectors from '../../data/dataFilmDirectors.json';
 
 const InitAuthorOfDay = () => {
     const { t } = useTranslation();
+    const authorOfDay = dataFilmDirectors[0];
+    console.log(authorOfDay);
     return (
         <div className="wrapper">
-            <div className="Author_of_day">
-
+            <div className="author">
+              <div className="author--short_description">
+                <img src={authorOfDay.photo} alt="author photo" className="author--short_description-img"></img>
+                <div className="author--short_description-name">{t('Khashchavatski name')} ({t('Khashchavatski city')})</div>
+                <div className="author--short_description-date">{authorOfDay.dateOfLife}</div>
+                <div className="author--short_description-info">{t('Khashchavatski description')}</div>
+              </div>
+              <div className="author--timeline">
+                <div className="author--timeline-biography">Biography</div>
+                <div className="author--timeline-container">
+                  <div className="entry">
+                    <div className="title">{authorOfDay.timeline[0].date}</div>
+                    <div className="information">{t(`Khashchavatski timeline ${0}`)}</div>
+                  </div>
+                  <div className="entry">
+                    <div className="title">{authorOfDay.timeline[1].date}</div>
+                    <div className="information">{t(`Khashchavatski timeline ${1}`)}</div>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
     );
