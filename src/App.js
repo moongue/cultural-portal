b5f05428adfa7348ae9f78f43922dd1de5b7ece2
moonGue <sasha.mojseenko2001@gmail.com>
2020-02-25 02:18:54 +0300

    feat: add page list film directors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component, Suspense } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 import Header from './components/Header/Header';
 import Greeting from './components/Greeting/Greeting';
+import FilmDirectorsList from './pages/FilmDirectorsList/FilmDirectorsList';
+import FilmDirector from './pages/FilmDirector/FilmDirector';
 
 class AppEl extends Component {
   render() {
     return (
       <>
         <Header />
-        <Greeting />
+        <div className="wrapper">
+          <Route path="/" exact>
+            <Greeting />
+          </Route>
+          <Route path="/film-directors" exact component={FilmDirectorsList} />
+          <Route path="/film-directors/:id" component={FilmDirector} />
+        </div>
       </>
     );
   }
@@ -18,8 +27,10 @@ const AppComponent = withTranslation()(AppEl);
 
 export default function App() {
   return (
-    <Suspense fallback="loading">
-      <AppComponent />
-    </Suspense>
+    <BrowserRouter>
+      <Suspense fallback="loading">
+        <AppComponent />
+      </Suspense>
+    </BrowserRouter>
   );
 }
