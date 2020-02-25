7783c4186902ce0b473fdd2fda1a109d13dfa116
Egor <Twinkstriker@gmail.com>
2020-02-26 01:22:00 +0300

    fix: styles, translate, directory

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import YouTube from '@u-wave/react-youtube';
 import './Player.scss';
 
 const Player = ( {youtube} ) => {
+  const { t } = useTranslation();
   const codeVideo = youtube.slice(-11, youtube.length);
   return (
     <div className='player'>
-      <div className='player--title'>Video</div>
+      <div className='player--title'>{t('Video')}</div>
       <YouTube 
         video = { codeVideo }
         className='video_player'
